e1f162406babbac1fa0427e306934bb1061dda41
prasadpund <46518467+prasadpund@users.noreply.github.com>
2019-01-09 13:42:25 +0530

    Add files via upload

---
+++ matting.py
@@ -0,0 +1,5 @@
+l=int(include('length is '))
+b=int(include('breadth is'))
+n=l*b/120
+print('no. of mats is')
+print(n)
---
+++ sum.py
@@ -0,0 +1,5 @@
+x=int(input('enter 1st number'))
+y=int(input('enter 2nd number'))
+z=x+y
+print('sum is')
+print(z)
